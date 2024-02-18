422fd7aa933a98a2eb47919806ede2df49ef9acb
uichi <37263474+uichi@users.noreply.github.com>
2024-02-18 21:03:46 +0900

    activerecord-last_modifiedのスケルトンを生成

---
+++ lib/activerecord/last_modified.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "last_modified/version"
+
+module Activerecord
+  module LastModified
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/last_modified/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Activerecord
+  module LastModified
+    VERSION = "0.1.0"
+  end
+end
